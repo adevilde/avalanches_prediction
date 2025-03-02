df57becc9b11577024a928bc02bea7671fb1f4ac
adevilde <117464736+adevilde@users.noreply.github.com>
2025-03-02 01:06:22 +0100

    Accuracy over 0.8. Have to clean the notebook and update download_data

--- submissions/starting_kit/estimator.py
+++ submissions/starting_kit/estimator.py
@@ -2,12 +2,39 @@
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OneHotEncoder
 
 
-def get_estimator():
-    pipe = make_pipeline(
-        StandardScaler(),
-        LogisticRegression()
+def get_estimator(df_final):
+    # Identify numerical and categorical features
+    num_features = df_final.select_dtypes(
+        include=['int64', 'float64']).columns.tolist()
+    cat_features = df_final.select_dtypes(
+        include=['object', 'category']).columns.tolist()
+
+    # Preprocessing Pipelines
+    num_pipeline = make_pipeline(
+        SimpleImputer(strategy='median'),
+        StandardScaler()
     )
 
-    return pipe
+    cat_pipeline = make_pipeline(
+        SimpleImputer(strategy='most_frequent'),
+        OneHotEncoder(handle_unknown='ignore')
+    )
+
+    # Combine preprocessing steps
+    preprocessor = ColumnTransformer([
+        ('num', num_pipeline, num_features),
+        ('cat', cat_pipeline, cat_features)
+    ])
+
+    # Define Model Pipeline
+    model_pipeline = make_pipeline(
+        preprocessor,
+        LogisticRegression(max_iter=1000)
+    )
+
+    return model_pipeline
